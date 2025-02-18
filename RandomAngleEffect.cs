4423ed06457b6a2005d5f2cb26dafe9fb870dec1
メタロロ <129175894+dmmo-com-jp@users.noreply.github.com>
2025-02-18 23:56:29 +0900

    Add files via upload

---
+++ RandomAngleEffect.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RandomAnglePlugin;
+using YukkuriMovieMaker.Commons;
+using YukkuriMovieMaker.Controls;
+using YukkuriMovieMaker.Exo;
+using YukkuriMovieMaker.Player.Video;
+using YukkuriMovieMaker.Plugin;
+using YukkuriMovieMaker.Plugin.Effects;
+
+namespace RandomAnglePlugin
+{
+    /// <summary>
+    /// 映像エフェクト
+    /// 映像エフェクトには必ず[VideoEffect]属性を設定してください。
+    /// </summary>
+    [VideoEffect("ランダム角度", ["アニメーション"], [])]
+    internal class RandomAngleEffect : VideoEffectBase
+    {
+        /// <summary>
+        /// エフェクトの名前
+        /// </summary>
+        public override string Label => "ランダム角度";
+
+        /// <summary>
+        /// アイテム編集エリアに表示するエフェクトの設定項目。
+        /// [Display]と[AnimationSlider]等のアイテム編集コントロール属性の2つを設定する必要があります。
+        /// [AnimationSlider]以外のアイテム編集コントロール属性の一覧はSamplePropertyEditorsプロジェクトを参照してください。
+        /// </summary>
+        [Display(Name = "X軸", Description = "X軸")]
+        [AnimationSlider("F0", "度", 0, 360)]
+        public Animation X軸 { get; } = new Animation(0, -10000, 10000);
+        [Display(Name = "Y軸", Description = "Y軸")]
+        [AnimationSlider("F0", "度", 0, 360)]
+        public Animation Y軸 { get; } = new Animation(0, -10000, 10000);
+        [Display(Name = "Z軸", Description = "Z軸")]
+        [AnimationSlider("F0", "度", 0, 360)]
+        public Animation Z軸 { get; } = new Animation(0, -10000, 10000);
+        [Display(Name = "間隔", Description = "間隔")]
+        [AnimationSlider("F2", "s", 0, 1)]
+        public Animation 間隔 { get; } = new Animation(0, -10000, 10000);
+        /// <summary>
+        /// Exoフィルタを作成する。
+        /// </summary>
+        /// <param name="keyFrameIndex">キーフレーム番号</param>
+        /// <param name="exoOutputDescription">exo出力に必要な各種情報</param>
+        /// <returns></returns>
+        public override IEnumerable<string> CreateExoVideoFilters(int keyFrameIndex, ExoOutputDescription exoOutputDescription)
+        {
+            //サンプルはSampleD2DVideoEffectを参照
+            return [];
+        }
+
+        /// <summary>
+        /// 映像エフェクトを作成する
+        /// </summary>
+        /// <param name="devices">デバイス</param>
+        /// <returns>映像エフェクト</returns>
+        public override IVideoEffectProcessor CreateVideoEffect(IGraphicsDevicesAndContext devices)
+        {
+            return new RandomAngleEffectProcessor(this);
+        }
+
+        /// <summary>
+        /// クラス内のIAnimatableを列挙する。
+        /// </summary>
+        /// <returns></returns>
+        protected override IEnumerable<IAnimatable> GetAnimatables() => [Z軸];
+        public PluginDetailsAttribute Details => new()
+        {
+            //制作者
+            AuthorName = "メタロロ",
+            //作品ID
+            ContentId = "",
+        };
+    }
+
+}
---
+++ RandomAngleEffectProcessor.cs
@@ -0,0 +1,81 @@
+﻿using RandomAnglePlugin;
+using System;
+using System.Windows.Media.Imaging;
+using Vortice.Direct2D1;
+using Vortice.DirectWrite;
+using YukkuriMovieMaker.Commons;
+using YukkuriMovieMaker.Player.Video;
+
+namespace RandomAnglePlugin
+{
+    internal class RandomAngleEffectProcessor : IVideoEffectProcessor
+    {
+        readonly RandomAngleEffect item;
+        ID2D1Image? input;
+
+        public ID2D1Image Output => input ?? throw new NullReferenceException(nameof(input) + "is null");
+
+        public RandomAngleEffectProcessor(RandomAngleEffect item)
+        {
+            this.item = item;
+        }
+
+        /// <summary>
+        /// エフェクトを更新する
+        /// </summary>
+        /// <param name="effectDescription">エフェクトの描画に必要な各種情報</param>
+        /// <returns>描画位置等の情報</returns>
+        public DrawDescription Update(EffectDescription effectDescription)
+        {
+            var frame = effectDescription.ItemPosition.Frame;
+            var length = effectDescription.ItemDuration.Frame;
+            var fps = effectDescription.FPS;
+            var drawDesc = effectDescription.DrawDescription;
+            var X軸 = item.X軸.GetValue(frame, length, fps);
+            var Y軸 = item.Y軸.GetValue(frame, length, fps);
+            var Z軸 = item.Z軸.GetValue(frame, length, fps);
+            var 間隔 = item.間隔.GetValue(frame, length, fps);
+            var seeds = frame + drawDesc.Draw.X + drawDesc.Draw.Y;
+            var fps_frame = frame % fps;
+            var fps間隔 = fps_frame % (fps / Math.Round(1 / 間隔));
+            if (fps間隔 != 0)
+            {
+                Random random2 = new Random((int)seeds - (int)fps間隔);
+                return
+                    drawDesc with
+                    {
+                        Rotation = new(
+                        drawDesc.Rotation.X + random2.Next(Math.Abs((int)X軸)) - ((int)X軸 / 2),
+                        drawDesc.Rotation.Y + random2.Next(Math.Abs((int)Y軸)) - ((int)Y軸 / 2),
+                        drawDesc.Rotation.Z + random2.Next(Math.Abs((int)Z軸)) - ((int)Z軸 / 2)
+                        )
+                    };
+            }
+
+            Random random = new Random((int)seeds);
+            return
+                drawDesc with
+                {
+                    Rotation = new (
+                        drawDesc.Rotation.X + random.Next(Math.Abs((int)X軸)) - ((int)X軸 / 2),
+                        drawDesc.Rotation.Y + random.Next(Math.Abs((int)Y軸)) - ((int)Y軸 / 2),
+                        drawDesc.Rotation.Z + random.Next(Math.Abs((int)Z軸)) - ((int)Z軸 / 2)
+                        )
+                };
+        }
+        public void ClearInput()
+        {
+            input = null;
+        }
+        public void SetInput(ID2D1Image? input)
+        {
+            this.input = input;
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+    }
+}
